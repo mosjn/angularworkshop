269fd9bbeb6800b939621711af4ea021b348bc02
mo <sujinun.kn@gmail.com>
2019-03-31 09:28:26 +0700

    updateimg

--- angularProject/src/app/app.component.ts
+++ angularProject/src/app/app.component.ts
@@ -9,17 +9,20 @@ export class AppComponent {
   productList = [{
     pId : "001",
     pName : "แปรงสีฟัน",
-    pCost : 200
+    pCost : 200,
+    img:"https://www.watsons.co.th/medias/sys_master/front/zoom/8910304641054.jpg"
   },
   {
     pId : "002",
     pName : "แก้วน้ำ",
-    pCost : 10
+    pCost : 10,
+    img:"../assets/img/แก้ว.jpg"
   },
   {
     pId : "003",
     pName : "ยาสีฟัน",
-    pCost : 20
+    pCost : 20,
+    img:"https://media.shopat24.com/pdmain/840227_020_AM_01.jpg"
   },
 ]
 selectedProduct :any;
